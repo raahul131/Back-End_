88a01a9ddeb06ecbf27f1c241ad2c873bea8045e
Rahul R Yadav <rryadav2025@gmail.com>
2023-05-27 15:44:56 +0530

    Intro complete

--- Node/Intro to node/server.js
+++ Node/Intro to node/server.js
@@ -1,10 +1,20 @@
-console.log(`hello world`);
+// console.log(`hello world`);
 
-const { chownSync, copyFileSync } = require('fs');
-const os = require('os');
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+// const { chownSync, copyFileSync } = require('fs');
+// const os = require('os');
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
+
+// const path = require(`path`);
+
+// console.log(path.parse(__filename));
+
+const {add, subtract, multiply, divide} = require('./math')
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
